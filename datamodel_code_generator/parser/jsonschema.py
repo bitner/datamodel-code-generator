62148715d251e0283f6ea87795680c0fba34a067
David Bitner <bitner@dbspatial.com>
2021-05-04 11:37:22 -0500

    Update jsonschema.py
    Add iri and iri-reference for JSON SCHEMA 7 support.

--- datamodel_code_generator/parser/jsonschema.py
+++ datamodel_code_generator/parser/jsonschema.py
@@ -92,6 +92,8 @@ json_schema_data_formats: Dict[str, Dict[str, Types]] = {
         'uuid5': Types.uuid5,
         'uri': Types.uri,
         'uri-reference': Types.string,
+        'iri': Types.uri,
+        'iri-reference': Types.string,
         'hostname': Types.hostname,
         'ipv4': Types.ipv4,
         'ipv6': Types.ipv6,
